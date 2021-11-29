4623657d4451574f72c4bb27323a7d5755247646
Volodymyr V. Marchenko <vmarchen@cisco.com>
2021-11-29 11:08:50 +0200

    add get-pip.py, jenkins/nginx.conf, pfctl, pip

---
+++ get-pip.py
@@ -0,0 +1,12 @@
+---
+tags: [ python, packaging ]
+---
+# Install pip via get-pip.py
+# Python 3
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+curl https://bootstrap.pypa.io/get-pip.py | python 
+
+# Python 2
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py  | python2x
+
